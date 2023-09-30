00d2f143e60fe0c3b427433c8c720a562b1ca52a
Omid Taghipour <o.taghipour@t2consult.net>
2023-09-30 15:59:25 +0200

    Changed education.jsx to use array mapping for education items.

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -5,27 +5,21 @@ import EducationItems from '../education-items/education-items';
 import TextAnimation from '../text-animation/text-animation';
 
 function Education() {
+    const educations = [
+        {id:1,logo:EMU, name:"B.S. in  Information Technology", uni: "Eastern Mediterranean University", duration:"Feb 2018 - Jul 2021", link:"https://www.emu.edu.tr/en/programs/information-technology-undergraduate-program/925"},
+        {id:2,logo:Kiel, name:"M.S. in Data Science", uni: "Kiel University of Applied Science", duration:"Sep 2022 - Present", link:"https://www.fh-kiel.de/en/degree-courses/courses/data-science/"},
+
+    ]
+
     return (
         <div className='education-container'>
             <TextAnimation tagName={'h1'} text={'education'} typingSpeed={100} classAtt={"title-text"} />
             <div className='education-items-container'>
-                <EducationItems
-                    logo={EMU}
-                    altText={"Eastern Mediterranean University"}
-                    title={"Eastern Mediterranean University"}
-                    desc={'B.S. in  Information Technology'}
-                    time={'Feb 2018 - Jul 2021'}
-                    href={'https://www.emu.edu.tr/en/programs/information-technology-undergraduate-program/925'}
-                />
-
-                <EducationItems
-                    logo={Kiel}
-                    altText={"Kiel University of Applied Sciences"}
-                    title={"Kiel University of Applied Sciences"}
-                    desc={'M.S. in Data Science'}
-                    time={'Sep 2022 - Present'}
-                    href={'https://www.fh-kiel.de/en/degree-courses/courses/data-science/'}
-                />
+                {
+                    educations.map(({id, logo, name, uni, duration, link}) => {
+                        return <EducationItems key={id} logo={logo} altText={uni} title={uni} desc={name} time={duration} href={link} />
+                    })
+                }
             </div>
         </div>
     )
